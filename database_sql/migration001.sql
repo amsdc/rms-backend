0999299af5492fa0db477168dc3c7887b27b2d5f
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-05-14 23:28:54 +0530

    Update migration001.sql
    * Added a default password hash

--- database_sql/migration001.sql
+++ database_sql/migration001.sql
@@ -38,4 +38,4 @@ CREATE TABLE orders_items (
 
 -- Insert sample data
 INSERT INTO users (username, password, user_type) VALUES ("__system", "", "admin"); -- future use
-INSERT INTO users (username, password, user_type) VALUES ("default", "", "manager");
+INSERT INTO users (username, password, user_type) VALUES ("default", "pbkdf2:sha256:260000$fstVNUjfKcKNVoH4$35cf1b3d453e80440b63cd09560d570d87606cbcf0ce3d38504c520d289add1e", "manager");
