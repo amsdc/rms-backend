79ba96486b1eea7472c84d35aab438c3d3a13f93
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-05-15 00:04:26 +0530

    Added list users options

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,5 +1,7 @@
 from flask_restful import Resource, abort
 
+from app import mysql
+
 # from app.models import Announcement, Achievement
 # from app.api.helpers import remove_html_tags
 
@@ -16,3 +18,18 @@ class Login(Resource):
                 "password": "passwd",
                 "user_type": "manager" or "customer"
             }
+
+class UserLists(Resource):
+    def get(self):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users")
+        users_tup = cur.fetchall()
+        ret_list = []
+        for user_tup in users_tup:
+            ret_list.append({
+                                "user_id": user_tup[0],
+                                "username": user_tup[1],
+                                "password": user_tup[2],
+                                "user_type": user_tup[3]
+                            })
+        return ret_list
--- app/api/urls.py
+++ app/api/urls.py
@@ -2,3 +2,4 @@ from app.api import app_api
 from app.api import endpoints as e
 
 app_api.add_resource(e.Login, "/login")
+app_api.add_resource(e.UserLists, "/users/all")
