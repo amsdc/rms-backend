e94f0e01fa04385ac79b4c4b1d5e73c9dda63360
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-05-18 11:41:30 +0530

    Correct SQL Statements
    * Move user creation to a new file
    * Correct user name to `aknp_rms`

--- database_sql/user_creation.sql
+++ database_sql/user_creation.sql
@@ -1,4 +1,4 @@
 -- Create a user
 CREATE USER 'aknp_rms'@'localhost' IDENTIFIED BY 'pa$$w0rd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'sammy'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'aknp_rms'@'localhost';
